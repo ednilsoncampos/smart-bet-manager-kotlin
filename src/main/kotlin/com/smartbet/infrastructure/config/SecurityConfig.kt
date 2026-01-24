7fa8e2fc7bd5978bf1e081ea1a018fd2d7219f9c
ednilsoncampos <ednilsoncampos@gmail.com>
2026-01-24 09:06:20 -0300

    fix: Libera acesso ao arquivo swagger.yaml

--- src/main/kotlin/com/smartbet/infrastructure/config/SecurityConfig.kt
+++ src/main/kotlin/com/smartbet/infrastructure/config/SecurityConfig.kt
@@ -49,6 +49,7 @@ class SecurityConfig(
                     
                     // Swagger/OpenAPI
                     .requestMatchers("/api-docs/**").permitAll()
+                    .requestMatchers("/api-docs.yaml").permitAll()
                     .requestMatchers("/swagger-ui/**").permitAll()
                     .requestMatchers("/swagger-ui.html").permitAll()
                     .requestMatchers("/v3/api-docs/**").permitAll()
