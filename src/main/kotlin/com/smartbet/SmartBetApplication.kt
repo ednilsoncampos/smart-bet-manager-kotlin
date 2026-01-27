f6c43623b9ffcfe40bd8800b9896b5286d90bfca
ednilsoncampos <ednilsoncampos@gmail.com>
2026-01-27 01:15:03 -0300

    fix: Adiciona melhorias para o log

--- src/main/kotlin/com/smartbet/SmartBetApplication.kt
+++ src/main/kotlin/com/smartbet/SmartBetApplication.kt
@@ -1,9 +1,12 @@
 package com.smartbet
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = [UserDetailsServiceAutoConfiguration::class]
+)
 class SmartBetApplication
 
 fun main(args: Array<String>) {
